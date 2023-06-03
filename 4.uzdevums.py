f8495f4eb64ff6a1233e175ef771c3a297844d6c
vabulsb <vabulsb@inbox.lv>
2023-06-03 10:24:05 +0300

    izmainas

---
+++ 4.uzdevums.py
@@ -0,0 +1,10 @@
+import math
+
+# Ievade riņķa rādiusa
+radius = int(input("Ievadiet riņķa rādiusu (vesels skaitlis): "))
+
+# Aprēķins riņķa laukumam
+area = math.pi * radius**2
+
+# Izvade riņķa laukumam
+print("Riņķa laukums ir:", area)
